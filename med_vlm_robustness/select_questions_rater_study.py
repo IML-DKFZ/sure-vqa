07cc2e32e71967cdc053d876619a3c3a4e3ae9ae
Kim-Celine Kahl <k.kahl@dkfz-heidelberg.de>
2024-10-09 12:47:01 +0200

    Delete old code

--- med_vlm_robustness/select_questions_rater_study.py
+++ med_vlm_robustness/select_questions_rater_study.py
@@ -7,44 +7,6 @@ import pandas as pd
 from utils import set_seed, get_config
 
 
-def select_questions_balanced(cfg):
-    set_seed(cfg.seed)
-    dataset = cfg.dataset
-    answer_type = cfg.answer_type
-    train_set = cfg.train_set
-    val_set = cfg.val_set
-    num_questions = cfg.num_questions
-
-    scores_path = Path(
-        f"{cfg.experiment_root_dir}/{dataset}/{cfg.finetune_method}/llava-{dataset}_train_{train_set}-finetune_{cfg.finetune_method}_{cfg.hyperparam_str}/eval/{dataset}_val_{val_set}/mistral_metrics.json")
-    with open(scores_path, 'r') as f:
-        scores = json.load(f)
-    scores_df = pd.DataFrame(scores[1:]).set_index("qid")
-    scores_df_non_equal = scores_df.loc[scores_df["gt"] != scores_df["pred"]]
-    num_ratings = []
-    num_ratings_2 = []
-    for score in range(1, 6):
-        num_ratings.append(len(scores_df_non_equal.loc[scores_df_non_equal["mistral_score"] == score]))
-        num_ratings_2.append(len(scores_df.loc[scores_df["mistral_score"] == score]))
-    json_file_path = cfg.dataset_dir / "validate.json"
-    output_file_path = cfg.output_dir / f"rater_study_{answer_type.lower()}_balanced.json"
-
-    with open(json_file_path, 'r') as f:
-        data = json.load(f)
-    data = [element for element in data if element.get("answer_type") == answer_type]
-    data = [element for element in data if element.get("qid") in list(scores_df_non_equal.index)]
-    selected_questions_id = []
-    for score in range(1, 6):
-        num_samples = int(num_questions / 5) if num_ratings[score-1] > num_questions / 5 else num_ratings[score-1]
-        random_qids = scores_df_non_equal.loc[(scores_df_non_equal["mistral_score"] == score)].sample(n=num_samples).index.values
-        selected_questions_id.extend(random_qids)
-    selected_questions = [element for element in data if element.get("qid") in selected_questions_id]
-    random.shuffle(selected_questions)
-    print(f"Number of selected questions: {len(selected_questions)}")
-    with open(output_file_path, 'w') as f:
-        json.dump(selected_questions, f, indent=4)
-
-
 def select_questions(cfg):
     set_seed(cfg.seed)
     dataset = cfg.dataset
